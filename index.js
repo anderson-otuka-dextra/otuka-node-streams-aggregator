2d505aa453257d93532139dd5fac2c51021c7c94
Anderson Otuka <anderson.otuka@dextra-sw.com>
2017-08-10 18:28:40 -0300

    Outputting stream to localhost server

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const http = require('http')
 
+let myRes
+
+const server = http.createServer((req, res) => {
+  myRes = res
+  myRes.writeHead(200, {'Content-Type': 'text/event-stream; charset=utf-8'});
+});
+
 const plugStream = (url, port, name) => {
   let streamData = ''
   const req = http.request({
@@ -13,8 +20,11 @@ const plugStream = (url, port, name) => {
       streamData += chunk
       let endOfStream = streamData.indexOf('\n\n')
       while (endOfStream > -1) {
-        console.log(`\ndata:{"name": "${name}", "url": "http://${url}:${port}",`)
-        console.log('"data":', streamData.substring(5, endOfStream), "}\n") // removes "data"
+        if (myRes) {
+          myRes.write(`\ndata:{"name": "${name}", "url": "http://${url}:${port}",`)
+          myRes.write('"data":' + streamData.substring(5, endOfStream) + "}\n")
+        }
+
         streamData = streamData.substring(endOfStream + 2)
         endOfStream = streamData.indexOf('\n\n')
       }
@@ -28,3 +38,6 @@ const plugStream = (url, port, name) => {
 
 plugStream('10.8.6.254', 3000, 'customer')
 plugStream('10.8.7.141', 3000, 'services')
+
+// Point your browser to http://localhost:3000 and see the magic
+server.listen(3000)
