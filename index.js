fca8590fd92356d7c1465bbc443e2b499f609c7e
Anderson Otuka <anderson.otuka@dextra-sw.com>
2017-08-10 19:32:23 -0300

    adding turbine urls

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ const server = http.createServer((req, res) => {
   myRes.writeHead(200, {'Content-Type': 'text/event-stream; charset=utf-8'});
 });
 
-const plugStream = (url, port, name) => {
+const plugStream = (url, port, path, name) => {
   let streamData = ''
   const req = http.request({
     hostname: url,
     port: port,
-    path: '/hystrix.stream',
+    path: path,
     method: 'get'
   }, (res) => {
     res.setEncoding('utf8')
     res.on('data', chunk => {
       streamData += chunk
+      if (streamData.indexOf(': ping') > -1) {
+        streamData = streamData.replace(/: ping\n/g, '')
+      }
       let endOfStream = streamData.indexOf('\n\n')
       while (endOfStream > -1) {
         if (myRes) {
@@ -30,14 +33,24 @@ const plugStream = (url, port, name) => {
       }
     })
     res.on('end', () => {
-      console.log('end')
+      res.end()
     })
   })
   req.end()
 }
 
-plugStream('10.8.6.254', 3000, 'customer')
-plugStream('10.8.7.141', 3000, 'services')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=realtimeconsumption', 'realtimeconsumption')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=attendancerecord', 'attendancerecord')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=featureflag', 'featureflag')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=services', 'services')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=bonus', 'bonus')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=plan', 'plan')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=customer', 'customer')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=authentication', 'authentication')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=sms', 'sms')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=billing', 'billing')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=payment', 'payment')
+plugStream('10.8.5.54', 8080, '/turbine.stream?cluster=mail', 'mail')
 
 // Point your browser to http://localhost:3000 and see the magic
 server.listen(3000)
